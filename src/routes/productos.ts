8f514768d6117c5c22a3fc28a2be4a808d726c1b
Jamutaq Ortega Vélez <jamutaqortegavelez@icloud.com>
2021-11-14 13:35:20 -0500

    mejora de la interfaz, se agregó función de búsqueda

--- src/routes/productos.ts
+++ src/routes/productos.ts
@@ -19,13 +19,13 @@ router.route("/create")
 		const url_img = req.file?.path; //obtiene la url de la imagen subida a Cloudinary
 		const { nombre, marca, precio_venta, precio_compra, pais_procedencia } = req.body;
 
-		await Producto.create({ 
-			nombre: nombre, 
-			marca: marca, 
+		await Producto.create({
+			nombre: nombre,
+			marca: marca,
 			precio_venta: precio_venta,
-			precio_compra: precio_compra, 
+			precio_compra: precio_compra,
 			pais_procedencia: pais_procedencia,
-			url_imagen: url_img 
+			url_imagen: url_img
 		});
 
 		//cuando se guarda el producto, que redireccione a la lista
@@ -35,7 +35,7 @@ router.route("/create")
 router.route("/list")
 	.get(async (req: Request, res: Response) => {
 		const productos = await Producto.find().lean();
-		console.log(productos);
+		// console.log(productos);
 		res.render("productos/list", { productos });
 	});
 
@@ -81,7 +81,7 @@ router.route("/edit/:id")
 				const url_components = (producto_data.url_imagen).split("/");
 				const id_img = url_components[8].split(".")[0];
 				const public_id = `${url_components[7]}/${id_img}`;
-			
+
 				//se elimina la imagen antigua de cloudinary
 				deleteImage(public_id);
 			}
@@ -89,20 +89,50 @@ router.route("/edit/:id")
 			const url_img = req.file.path;
 
 			//actualiza los datos del producto includa la imagen
-			await Producto.findByIdAndUpdate(id, { 
-				nombre: nombre, 
-				marca: marca, 
-				precio_venta: precio_venta, 
-				precio_compra: precio_compra, 
-				pais_procedencia: pais_procedencia, 
-				url_imagen: url_img 
+			await Producto.findByIdAndUpdate(id, {
+				nombre: nombre,
+				marca: marca,
+				precio_venta: precio_venta,
+				precio_compra: precio_compra,
+				pais_procedencia: pais_procedencia,
+				url_imagen: url_img
 			});
 		}
 
 		//sino solo actualiza los demas datos sin img
 		await Producto.findByIdAndUpdate(id, { nombre, marca, precio_venta, precio_compra, pais_procedencia });
-		
+
 		res.redirect("/productos/list");
+	});
+
+router.route("/search")
+	.get((req: Request, res: Response) => {
+		res.render("productos/search");
+	})
+	.post(async (req: Request, res: Response) => {
+		//obtiene el texto que se quiere buscar
+		const txt: string = req.body.frmBuscar;
+		//convierte el texto a minuscula para una mejor busqueda
+		const keyword = txt.toLowerCase(); 
+		
+		//obtiene todos los productos
+		const productos = await Producto.find().lean();
+
+		//guarda los productos encontrados
+		const prods = [];
+
+		//itera cada objeto en el array de objetos que se obtuvo de la BD
+		for (const producto of productos) {
+			//convierte el nombre del producto a minus para una mejor busqueda
+			const nombre = producto.nombre.toLowerCase();
+			
+			//si el keyword tiene cierta coincidencia con el nombre
+			if (nombre.indexOf(keyword) !== -1) {
+				prods.push(producto); //guarda los resultados
+			}
+		}
+
+		res.render("productos/search", { prods });
 	});
 
 export default router;
